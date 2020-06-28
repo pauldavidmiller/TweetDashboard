9a3e8e9c672d264abc7f917a3baace46f7d15788
Paul Miller <millsdabomb@gmail.com>
2020-06-27 21:05:09 -0400

    Added elements to add/delete groupings and then add twitter timelines under those groupings - having trouble retrieving users from database under grouping though *need to fix*

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { UserListService } from './userlist.service';
 import { UserService } from '../user/user.service';
 import { UserComponent } from '../user/user.component';
@@ -13,20 +13,22 @@ export class UserListComponent implements OnInit {
   title = "Add Twitter Timelines and Build Your Dashboard!";
   users;
   groups;
+  selectedGroups: Array<string> = []; // all selected groups - only one can be selected to have users added
 
-  constructor(public service: UserListService) { 
+  constructor(public service: UserListService) {
+    //var groupToGetTEMP = "Friends";
     // Get Users from Database
-    this.users = service.getUsers();
+    //this.users = service.getUsers(groupToGetTEMP);
     // Get Groups from Database
     this.groups = service.getGroups();
-    // Switch to "All" group initially
-    this.changeGroup("All");
+    // Start with all groups selected initially
+    // this.changeGroup(groupToGetTEMP);
   }
 
   ngOnInit(): void {
   }
 
-  addUser(newUser: string){
+  addUser(newUser: string) {
 
     // Update list
     if (newUser) {
@@ -34,7 +36,7 @@ export class UserListComponent implements OnInit {
     }
 
     // Update database
-    this.service.updateUsers(this.users);
+    this.service.updateUsers(this.users, this.selectedGroups[0]);
 
     console.log(this.users)
   }
@@ -42,7 +44,7 @@ export class UserListComponent implements OnInit {
   removeUser(User: string) {
 
     // Update list
-    if (User){
+    if (User) {
       // Get index of user
       let index = this.users.indexOf(User);
       this.users.splice(index, 1);
@@ -52,13 +54,13 @@ export class UserListComponent implements OnInit {
     }
 
     // Update database
-    this.service.updateUsers(this.users);
+    this.service.updateUsers(this.users, this.selectedGroups[0]);
   }
 
 
-  addGroup(newGroup: string){
+  addGroup(newGroup: string) {
     // Add Group to Page and Database
-    if (newGroup){
+    if (newGroup) {
       this.groups.push(newGroup);
     }
 
@@ -69,9 +71,9 @@ export class UserListComponent implements OnInit {
 
   }
 
-  removeGroup(Group: string){
+  removeGroup(Group: string) {
     // Update list
-    if (Group){
+    if (Group) {
       // Get index of group
       let index = this.groups.indexOf(Group);
       this.groups.splice(index, 1);
@@ -85,12 +87,41 @@ export class UserListComponent implements OnInit {
   }
 
 
-  changeGroup(Group: string){
-    // Change display of group
-    if (Group){
-      // Change border color
-      
-      // Change timelines
+  changeGroup(selected: boolean, Group: string) {
+    // Check which groups are selected and are in the group list
+    if (selected) {
+      // Add groups to list if selected
+      this.selectedGroups.push(Group);
+    } else {
+      // Remove groups from list if deselected
+      var index = this.selectedGroups.indexOf(Group);
+      console.log("index of selected false: " + index);
+      this.selectedGroups.splice(index, )
+      this.selectedGroups.splice(index, 1);
     }
+
+    // Change display of selected groups
+    var allUsers: Array<string> = [];
+    for (let i = 0; i < this.selectedGroups.length; i++) {
+      const group = this.selectedGroups[i];
+      var groupUsers = this.service.getUsers(group.toString());
+
+      console.log("groupUsers");
+      console.log(groupUsers);
+
+      // TODO: FIX THIS
+
+      for (let j = 0; j < groupUsers.length; j++) {
+        const user = groupUsers[j];
+        console.log("user");
+        console.log(user);
+        allUsers.push(user.toString());
+      }
+    }
+
+    this.users = allUsers;
+  
+
+    //console.log("selected groups: " + this.selectedGroups);
   }
 }
--- src/app/userlist/userlist.service.ts
+++ src/app/userlist/userlist.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 //import { of as observableOf, Observable } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { map } from "rxjs/operators";
+import { map, elementAt } from "rxjs/operators";
 import { auth } from "firebase";
 import { AngularFireDatabase } from 'angularfire2/database';
 import * as firebase from 'firebase';
@@ -18,25 +18,24 @@ export class UserListService {
         //let n = this.db.list('clients').valueChanges();
     }
 
-    getUsers() {
+    getUsers(Group: string) {
 
         // Try catch block to check uid is logged in - if no login give clean slate for playground
         try {
             let uid = this.auth.auth.currentUser.uid;
             console.log("getUsers: " + uid);
 
+            // Do fetch of data
             var list: Array<string> = [];
-            var ref = firebase.database().ref().child('/clients/' + uid + '/users/').orderByChild('users');
+            var ref = firebase.database().ref().child('/clients/' + uid + '/groups/' + Group);
             ref.once('value', function (snap) {
                 snap.forEach(function (item) {
                     var itemVal = item.val();
                     list.push(itemVal);
-                    console.log(itemVal);
                 });
             });
 
             return list;
-
 
         } catch (e) {
             console.log(e);
@@ -47,15 +46,34 @@ export class UserListService {
 
     getGroups() {
         // Get groups from database under the user signed in
+        // Try catch block to check uid is logged in - if no login give clean slate for playground
         try {
-            return ["All", "Politicians"];
+            let uid = this.auth.auth.currentUser.uid;
+            console.log("getGroups: " + uid);
+
+            var list: Array<string> = [];
+            var ref = firebase.database().ref('/clients/' + uid + '/groups/').orderByChild('groups');
+            ref.once('value', function (snap) {
+                snap.forEach(function (item) {
+                    var itemVal = item.key;
+                    list.push(itemVal);
+                });
+            });
+
+            console.log("getGroups List: " + list);
+
+            return list;
+
+
         } catch (e) {
+            console.log(e);
+
             return ["All"];
         }
     }
 
     // Update the users per account in the database
-    updateUsers(users: any) {
+    updateUsers(users: any, Group: string) {
 
         // Try catch block to check uid is logged in
         try {
@@ -63,7 +81,7 @@ export class UserListService {
             console.log("updateUsers: " + uid);
 
             // Update user list in database
-            firebase.database().ref('clients/' + uid + '/users/').set(users);
+            firebase.database().ref('clients/' + uid + '/groups/' + Group).set(users);
 
 
         } catch (e) {
