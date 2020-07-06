a36aedb4a61998ce94c6881364aa6986c525556d
Paul Miller <millsdabomb@gmail.com>
2020-07-06 02:24:31 -0400

    Added buttons to sign in with facebook and username/email. Need to debug these buttons and their respective signin.

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -40,23 +40,16 @@ export class ClientService {
     //let n = this.db.list('clients').valueChanges();
   }
 
-  login() {
+  loginGoogle() {
     let login = this.auth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(function (result) {
       // The signed-in user info.
       let user = result.user;
-      console.log(user.email);
-      console.log(user.uid);
-
       var postData = {
         email: user.email
       };
 
-
       // UPDATE WITH KEY
       // https://firebase.google.com/docs/database/web/read-and-write
-      //var newPostKey = firebase.database().ref().child('clients').push().key;
-      //var updates = {};
-      //updates['/clients/' + user.uid + '/'] = postData;
       let info = firebase.database().ref('clients/' + user.uid).update(postData);
       console.log(info);
 
@@ -70,8 +63,55 @@ export class ClientService {
       var credential = error.credential;
     });
 
-
     //this.auth.auth.signInWithEmailAndPassword()
+  }
+
+  loginFacebook() {
+    let login = this.auth.auth.signInWithPopup(new auth.FacebookAuthProvider()).then(function (result) {
+      // The signed-in user info.
+      let user = result.user;
+      var postData = {
+        email: user.displayName
+      };
+
+      // UPDATE WITH KEY
+      // https://firebase.google.com/docs/database/web/read-and-write
+      let info = firebase.database().ref('clients/' + user.uid).update(postData);
+      console.log(info);
+
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+    });
+  }
+
+  loginEmailPassword() {
+    // let login = this.auth.auth.signInWithEmailAndPassword().then(function (result) {
+    //   // The signed-in user info.
+    //   let user = result.user;
+    //   var postData = {
+    //     email: user.email
+    //   };
+
+    //   // UPDATE WITH KEY
+    //   // https://firebase.google.com/docs/database/web/read-and-write
+    //   let info = firebase.database().ref('clients/' + user.uid).update(postData);
+    //   console.log(info);
+
+    // }).catch(function (error) {
+    //   // Handle Errors here.
+    //   var errorCode = error.code;
+    //   var errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   var email = error.email;
+    //   // The firebase.auth.AuthCredential type that was used.
+    //   var credential = error.credential;
+    // });
   }
 
   logout() {
