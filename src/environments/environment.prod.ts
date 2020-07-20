278f96957723ad4d481b81c826964188b283b629
Paul Miller <millsdabomb@gmail.com>
2020-07-20 01:15:55 -0400

    Production build for web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAScGiq-r7pUqICf79PA9IGlLwoUfqCkcc",
+    authDomain: "tweetdashboard.firebaseapp.com",
+    databaseURL: "https://tweetdashboard.firebaseio.com",
+    projectId: "tweetdashboard",
+    storageBucket: "tweetdashboard.appspot.com",
+    messagingSenderId: "238084112616",
+    appId: "1:238084112616:web:372cb613372ccd55aa895d",
+    measurementId: "G-16XRX5TVYT"
+  }
 };
